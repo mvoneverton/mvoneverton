37bff417babda472dc7b62bf4a64469e976a1c43
Mike Everton <mvoneverton@gmail.com>
2016-03-01 15:01:13 -0700

    update

--- src/index.js
+++ src/index.js
@@ -14,7 +14,9 @@ $(document).ready(function() {
 		window.open(project + projectID, "_blank");
 	});
 
-	$(".contact-form").on("submit", function () {
+	$(".contact-form").on("submit", function (e) {
+
+		e.preventDefault();
 		 
     var name = $(".name").val();
     var email = $(".email").val();

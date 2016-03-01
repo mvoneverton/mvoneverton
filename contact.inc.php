99a081a9475b851db272ea02efe89d4aded54678
Mike Everton <mvoneverton@gmail.com>
2016-03-01 14:49:28 -0700

    update

--- contact.inc.php
+++ contact.inc.php
@@ -15,6 +15,7 @@ if ($dbconn->connect_error) {
 } 
 
 // Save input to the database
+
 $nameval = $_POST["name1"];
 $emailval = $_POST["&email"];
 $phoneval = $_POST["&phone"];
@@ -22,6 +23,11 @@ $messageval = $_POST["&message"];
 $sql = "INSERT INTO user (name, email, phone, message)
 VALUES ('$nameval', '$emailval', '$phoneval', '$messageval')";
 
+if($_POST["sendbutton"]){
+	$q = $dbconn->prepare("INSERT INTO user (name, email, phone, message)
+				VALUES ('$nameval', '$emailval', '$phoneval', '$messageval')";
+				$q->execute(array($_POST["name"], $_POST["email"], $_POST["phone"], $_POST["message"])));
+}
 
 if ($dbconn->query($sql) == True) {
 	echo "New record created successfully";
